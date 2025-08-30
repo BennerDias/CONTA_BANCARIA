048802cc6ac301f6b21d7d1c24f63005323da622
benner.dias <bennerheberth@gmail.com>
2025-08-30 19:04:21 -0300

    Menu de selecoes, done

--- Menu/Menu.ts
+++ Menu/Menu.ts
@@ -1,18 +1,59 @@
-console.log("*****************************************************");
-console.log("                                                     ");
-console.log("            BANCO DO BRAZIL COM Z                    ");
-console.log("                                                     ");
-console.log("*****************************************************");
-console.log("                                                     ");
-console.log("            1 - Criar Conta                          ");
-console.log("            2 - Listar todas as Contas               ");
-console.log("            3 - Buscar Conta por Numero              ");
-console.log("            4 - Atualizar Dados da Conta             ");
-console.log("            5 - Apagar Conta                         ");
-console.log("            6 - Sacar                                ");
-console.log("            7 - Depositar                            ");
-console.log("            8 - Transferir valores entre Contas      ");
-console.log("            9 - Sair                                 ");
-console.log("                                                     ");
-console.log("*****************************************************");
-console.log("Entre com a opção desejada: ");
+import { Colors } from './../src/util/Colors/Colors';
+import * as leia from 'readline-sync';
+
+console.log(Colors.FgYellow + `
+*****************************************************
+                                                    
+            BANCO DO BRAZIL COM Z                    
+                                                    
+*****************************************************
+                                                    
+            1 - Criar Conta                          
+            2 - Listar todas as Contas               
+            3 - Buscar Conta por Numero              
+            4 - Atualizar Dados da Conta             
+            5 - Apagar Conta                         
+            6 - Sacar                                
+            7 - Depositar                            
+            8 - Transferir valores entre Contas      
+            9 - Sair                                 
+                                                    
+*****************************************************
+Entre com a opção desejada:
+` );
+
+let opcao: number = leia.questionInt('Digite a opcao desejada: ')
+
+function MenuSelection(opcao: number){
+    switch(opcao){
+        case 1:
+            console.log('Criar conta...')
+            break
+        case 2:
+            console.log('Listar todas as Contas...')
+            break
+        case 3:
+            console.log('Buscar conta por numero...')
+            break
+        case 4:
+            console.log('Atualizar dados da conta...')
+            break
+        case 5:
+            console.log('Apagar conta...')
+            break
+        case 6:
+            console.log('Sacar...')
+            break
+        case 7:
+            console.log('Depositar...')
+            break
+        case 8:
+            console.log('Transferir valores entre contas...')
+            break
+        case 9:
+            console.log('Saindo...')
+        default:
+            console.log('Opção inválida, seleciona uma opção válida de 1-9')
+    }
+}
+MenuSelection(opcao);
